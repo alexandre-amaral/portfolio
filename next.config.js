cc30d8df9fae0b4f2a38efd78be4430807ecd386
Alexandre Amaral | Grupo Via Capital <alexandre.amaral@grupoviacapital.com>
2025-03-06 16:24:43 -0300

    Add GitHub Actions workflow for deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
   images: {
     unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
   trailingSlash: true,
-  distDir: 'out',
 };
 
 module.exports = nextConfig; 
